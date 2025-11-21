efe4f9f13d6fa4148c452d08df9526b8bd31893b
Yoonsuk Oh <yun104487@ajou.ac.kr>
2025-11-21 19:43:48 +0900

    시리얼 통신 코드 추가

---
+++ 01_Code/1121_Serial_Python.py
@@ -0,0 +1,11 @@
+import serial
+import time
+py_serial = serial.Serial(port='COM8', baudrate=9600,)
+
+while True:
+    comment = input("아두이노에게 내릴 명령: ")
+    py_serial.write(commend.encode())
+    time.sleep(0.1)
+    if py_serial.readable():
+        response = py_serial.readline()
+        print(response[:len(response)-1]/decode())
